e4a6540937d7c7ca74c2cb8f5967efc142fbb74f
Ishimwe Divine <27212@example.com>
2026-02-02 13:50:11 +0200

    Initial commit for servlet assignment

---
+++ src/main/Java/auca.ac.rw/LoginServlet.java
@@ -0,0 +1,29 @@
+
+package auca.ac.rw;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        java.io.PrintWriter out = response.getWriter();
+
+        if (password.length() < 8) {
+            out.println("Hello " + username + ", your password is weak. Try a strong one.");
+        } else {
+            out.println("Welcome " + username);
+        }
+    }
+}
